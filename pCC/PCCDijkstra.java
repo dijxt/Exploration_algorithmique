de4aa14226c3dce1d5442214dee4150ffec6ab39
Ilyes Abbassi <ilyes.abbassi@gmail.com>
2022-04-09 19:26:23 +0200

    Debut test

--- pCC/PCCDijkstra.java
+++ pCC/PCCDijkstra.java
@@ -3,7 +3,7 @@ package pCC;
 import graphes.IGraph;
 
 public class PCCDijkstra {
-    public static int[] PCCDijkstra(IGraph g, int source, int cible){
+    public static int[] PCCDijkstra(IGraph g, String source, String cible){
 
         // TODO code + return
         return new int[0];
