7a91a318d6b7352034fd7fac6609c99100697b91
Ilyes Abbassi <ilyes.abbassi@gmail.com>
2022-04-17 01:26:24 +0200

    dijkstra

--- pCC/PCCDijkstra.java
+++ pCC/PCCDijkstra.java
@@ -1,9 +1,75 @@
 package pCC;
 
-public class PCCDijkstra {
-    public static int[] PCCDijkstra(IGraph g, String source, String cible){
+import graphes.ArcNEégatifEx;
 
-        // TODO code + return
-        return new int[0];
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PCCDijkstra {
+
+    private final IGraph g;
+    private final String source;
+    private final String cible;
+    private ArrayList<String> noeudsVisites;
+    private int[] distanceSommets;
+    private String[] dernierPredecesseur;
+
+
+    /**
+     * Indique si un graphe peut être soumis à l'algorithme de dijkstra
+     * @param g le graphe
+     * @return true si le graphe est correct, false sinon
+     */
+    private static boolean estOkGraphe(IGraph g){
+        for (String s: g.getLabels()){
+            for (String sc: g.getSuccesseurs(s)){
+                if (g.getValeur(s, sc) < 0){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Constructeur de la classe PCCDijkstra.
+     * @param g un graphe
+     * @param source un sommet source
+     * @param cible un sommet cible
+     */
+    public PCCDijkstra(IGraph g, String source, String cible){
+        assert estOkGraphe(g);
+        this.g = g;
+        this.source = source;
+        this.cible = cible;
+        this.noeudsVisites = new ArrayList<>();
+        this.distanceSommets = new int[g.getNbNoeuds()];
+        this.dernierPredecesseur = new String[g.getNbNoeuds()];
+
+        noeudsVisites.add(source);
+        distanceSommets[0] = 0;
+        dernierPredecesseur[0] = null;
+
+        for(int i = 1; i < g.getNbNoeuds(); ++i) {
+            if (Arrays.asList(g.getSuccesseurs(source)).contains(g.getLabels()[i])) {
+                distanceSommets[i] = g.getValeur(source, g.getLabels()[i]);
+                dernierPredecesseur[i] = source;
+            } else {
+                distanceSommets[i] = Integer.MAX_VALUE;
+                dernierPredecesseur[i] = null;
+            }
+        }
+    }
+
+    /**
+     * Renvoie le plus court chemin entre deux sommets d'un graphe
+     * @return le plus court chemin
+     */
+    public String[] PCC(){
+
+        while (!noeudsVisites.contains(cible)){
+
+        }
+        return new String[0];
     }
 }
--- tests/pCCTest/PCCTest.java
+++ tests/pCCTest/PCCTest.java
@@ -1,7 +1,67 @@
 package tests.pCCTest;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import static org.junit.Assert.*;
 
-public class PCCTest {
+import graphes.GrapheLA;
+import graphes.GrapheMA;
 
+import pCC.IGraph;
+import pCC.PCCDijkstra;
+
+public class PCCTest {
+    @Test
+    void test() {
+        String[] noeuds = {"A", "B", "C", "D", "E", "F", "G", "H", "I"};
+        IGraph g = new GrapheMA(noeuds);
+        tester(g);
+        testerPCC(g);
+        g = new GrapheLA(noeuds);
+        tester(g);
+        testerPCC(g);
+    }
+
+    void tester(IGraph g) {
+        assertEquals(9, g.getNbNoeuds());
+        g.ajouterArc("A", "C", 2);
+        g.ajouterArc("A", "D", 1);
+        g.ajouterArc("B", "G", 3);
+        g.ajouterArc("C", "H", 2);
+        g.ajouterArc("D", "B", 3);
+        g.ajouterArc("D", "C", 5);
+        g.ajouterArc("D", "E", 3);
+        g.ajouterArc("E", "C", 1);
+        g.ajouterArc("E", "G", 3);
+        g.ajouterArc("E", "H", 7);
+        g.ajouterArc("G", "B", 2);
+        g.ajouterArc("G", "F", 1);
+        g.ajouterArc("H", "F", 4);
+        g.ajouterArc("H", "G", 2);
+        g.ajouterArc("I", "H", 10);
+    }
+
+    void testerPCC(IGraph g){
+
+        PCCDijkstra p1 = new  PCCDijkstra(g, "A", "H");
+        Assertions.assertEquals(p1.PCC(), new String[]{"A", "C", "H"});
+
+        PCCDijkstra p2 = new  PCCDijkstra(g, "A", "G");
+        Assertions.assertEquals(p2.PCC(), new String[]{"A", "C", "H", "G"});
+
+        PCCDijkstra p3 = new  PCCDijkstra(g, "A", "F");
+        Assertions.assertEquals(p3.PCC(), new String[]{"A", "C", "H", "G", "F"});
+
+        PCCDijkstra p4 = new  PCCDijkstra(g, "A", "E");
+        Assertions.assertEquals(p4.PCC(), new String[]{"A", "D", "E"});
+
+        PCCDijkstra p5 = new  PCCDijkstra(g, "A", "D");
+        Assertions.assertEquals(p5.PCC(), new String[]{"A", "D"});
+
+        PCCDijkstra p6 = new  PCCDijkstra(g, "A", "C");
+        Assertions.assertEquals(p6.PCC(), new String[]{"A", "C"});
+
+        PCCDijkstra p7 = new  PCCDijkstra(g, "A", "B");
+        Assertions.assertEquals(p7.PCC(), new String[]{"A", "D", "B"});
+    }
 }
