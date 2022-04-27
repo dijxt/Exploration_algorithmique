a7911ae52a2a887f5eaa618dcb9743c000c1cc47
Ali-Thi <ali.talhaoui95@gmail.com>
2022-04-27 14:50:08 +0200

    first try algo bellman

--- pCC/PCCBellman.java
+++ pCC/PCCBellman.java
@@ -1,54 +1,27 @@
 package pCC;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.*;
 
 public class PCCBellman {
 
-    /*private static boolean triTopologiqueEffectuable (IGraph g){
-        ArrayList<String> labels = new ArrayList<>();
-        for (String label : g.getLabels()){
-            if (g.dIn(label) == 0){
-                for (String successeur : )
-            }
+    public static int PCC(IGraph g, String source, String cible){
+        Map<String, Integer> distances = new HashMap<>();
+        Map<String, String> predecesseurs = new HashMap<>();
+
+        for (String tmpIterNext : g) {
+            distances.put(tmpIterNext, Integer.MAX_VALUE);
+            predecesseurs.put(tmpIterNext, null);
         }
-    }*/
 
-    private static void triTopologique(IGraph g){
-        int n = g.getNbNoeuds();
-        int[] rang = new int[n];
-        int[] degre = new int[n];
-        String[] labels = g.getLabels();
-        LinkedList<String> L = new LinkedList<String>();
-        ArrayList<String> X = new ArrayList<String>();
+        distances.put(source, 0);
+        for (String label1 : distances.keySet())
+            for (String label2 : distances.keySet())
+                if (label1 != source || label2 != source || label2 != label1)
+                    if (distances.get(label2) > distances.get(label1) + g.getValeur(label1, label2)){
+                        distances.put(label2, distances.get(label1) + g.getValeur(label1, label2));
+                        predecesseurs.put(label2, label1);
+                    }
 
-        Arrays.fill(rang, 0);
-        for (int i = 0 ; i < L.size() ; ++i)
-            degre[i] = g.dIn(L.get(i));
-
-        while (X.size() < labels.length){
-            for (String label : labels){
-                if (degre[Arrays.binarySearch(labels, label)] == 0)
-                    L.add(label);
-            }
-
-            for (String label : L){
-                for (String successeur : g.getSuccesseurs(label)) {
-                    if (rang[L.indexOf(successeur)] < rang[L.indexOf(label)]+1)
-                        rang[L.indexOf(successeur)] = rang[L.indexOf(label)]+1;
-
-                    degre[L.indexOf(successeur)] = degre[L.indexOf(label)]+1;
-                }
-            }
-            X.addAll(L);
-            L.clear();
-        }
-    }
-
-    public static int[] PCCBellman(IGraph g, int source, int cible){
-
-        // TODO code + return
-        return new int[0];
+        return distances.get(cible);
     }
 }
