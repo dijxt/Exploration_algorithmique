fd34f897011b87afc0ae30a6374534281b23a142
Ali-Thi <ali.talhaoui95@gmail.com>
2022-04-30 14:51:22 +0200

    second try algo bellman

--- pCC/PCCBellman.java
+++ pCC/PCCBellman.java
@@ -5,59 +5,57 @@ import graphes.CircuitEx;
 import java.util.*;
 
 public class PCCBellman {
-/*
-    public static int[] PCC(IGraphe g, int source, int cible){
+
+    public static int[] PCC(IGraphe g, int source, int cible) {
         int[] distances = new int[g.getNbSommets()];
-        int[] rangs = triTopologique(g);
+        int[] predecesseurs = new int[g.getNbSommets()];
+        List<Integer> sommetsParRang = triTopologique(g);
 
-        for (int i = 0 ; i < g.getNbSommets() ; i++) {
-            if (rangs[i] == 0)
-                distances[i] = 0;
-            else
-                distances[i] = Integer.MAX_VALUE;
+        Arrays.fill(distances, Integer.MAX_VALUE);
+        Arrays.fill(predecesseurs, (Integer) null);
+
+        distances[sommetsParRang.get(0)] = 0;
+
+        for (Iterator<Integer> it = sommetsParRang.iterator(); it.hasNext(); ) {
+            int i = it.next();
+            for (int j : g.getSuccesseurs(i))
+                if (distances[j] > distances[i] + g.getValuation(i, j)) {
+                    distances[j] = distances[i] + g.getValuation(i, j);
+                    predecesseurs[j] = i;
+                }
         }
-
-        for (int i = 0 ; i < g.getNbSommets() ; i++)
-            for (int j )
-                if (i != source || j != source || i != j)
-                    if (distances[j] > distances[i] + g.getValuation(i, j)){
-                        distances[j] = distances[i] + g.getValuation(i, j);
-                        predecesseurs[j] = i;
-                    }
-
-        if (!estOkGraphe(predecesseurs, source) || predecesseurs.length != g.getNbSommets())
-            throw new CircuitEx();
 
         List<Integer> pcc = new ArrayList<>();
         pcc.add(cible);
-        while(pcc.get(pcc.size() - 1) != source)
+        while (pcc.get(pcc.size() - 1) != source)
             pcc.add(predecesseurs[pcc.get(pcc.size() - 1)]);
 
-        while(pcc.get(0) != source)
+        while (pcc.get(0) != source)
             pcc.add(pcc.remove(0));
 
         return pcc.stream().mapToInt(Integer::intValue).toArray();
     }
 
 
-    private static int[] triTopologique(IGraphe g){
+    private static List<Integer> triTopologique(IGraphe g) {
         Map<Integer, List<Integer>> predecesseurs = new HashMap<>();
-        for (int i = 0 ; i < g.getNbSommets() ; i++) {
+        for (int i = 0; i < g.getNbSommets(); i++) {
             predecesseurs.put(i, new ArrayList<Integer>());
             for (int j : g.getSuccesseurs(i))
                 predecesseurs.get(i).add(j);
         }
 
-        int[] rangs = new int[g.getNbSommets()];
+        List<Integer> sommetsParRangs = new ArrayList<>();
         List<Integer> sommetsOut = new ArrayList<>();
-        for (int i = 0 ; i < g.getNbSommets() ; i++)
+        for (int i = 0; i < g.getNbSommets(); i++)
             for (int key : predecesseurs.keySet()) {
-                if (predecesseurs.get(key).isEmpty() && g.getSuccesseurs(key).length > 0) {
-                    rangs[key] = 0;
+                if (predecesseurs.get(key).isEmpty() && sommetsParRangs.isEmpty()) {
+                    sommetsParRangs.add(key);
                     sommetsOut.add(key);
-                }
-                else if (sommetsOut.contains(predecesseurs.get(key)))
-                    rangs[key] = i;
+                    break;
+                } else if (sommetsOut.contains(predecesseurs.get(key)))
+                    sommetsParRangs.add(key);
             }
-        return rangs;*/
+        return sommetsParRangs;
     }
+}
